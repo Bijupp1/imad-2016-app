a005819df779ac1e435279a399ac31b88ce1c924
Bijupp1 <writetoppbiju@gmail.com>
2016-10-08 17:55:41 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send("Article Two requested and will be served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-Three',function(req,res){
-    res.send("Article Three requested and will be served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
